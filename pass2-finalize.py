3b7c951e76fafb393e73d8ccb8f37ebc323a1304
be5invis <belleve@typeof.net>
2015-08-18 06:38:58 +0800

    Rebuilt /U, /J, /m, /n, etc... with spiros.

--- pass2-finalize.py
+++ pass2-finalize.py
@@ -6,11 +6,7 @@ font = fontforge.open(source)
 
 font.selection.all()
 font.removeOverlap()
-font.em = 1000
-font.round()
-font.removeOverlap()
 font.simplify(1)
-font.addExtrema()
 font.canonicalContours()
 font.canonicalStart()
 font.generate(sys.argv[2], flags = ("short-post", "opentype"))
