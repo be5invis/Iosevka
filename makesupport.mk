3a53647cfd3ccf5805e685248010bea7791945f4
be5invis <belleve@typeof.net>
2015-12-13 05:50:02 +0800

    Change extension of the ".patel" files to ".aki".

--- makesupport.mk
+++ makesupport.mk
@@ -1,21 +1,21 @@
-PATEL = node ./node_modules/patel/bin/patel-c
-SUPPORT_FILES_FROM_PATEL = support/glyph.js support/stroke.js support/spiroexpand.js support/spirokit.js parameters.js
-SUPPORT_FILES = $(SUPPORT_FILES_FROM_PATEL) support/point.js extract.js generate.js emptyfont.toml parameters.toml
-GLYPH_SEGMENTS = glyphs/common-shapes.patel glyphs/overmarks.patel glyphs/letters-unified-basic.patel glyphs/letters-unified-extended.patel  glyphs/numbers.patel glyphs/symbol-punctuation.patel glyphs/symbol-math.patel glyphs/symbol-geometric.patel glyphs/symbol-other.patel glyphs/symbol-letter.patel glyphs/autobuilds.patel
+PATELC = node ./node_modules/patel/bin/patel-c
+SUPPORT_FILES_FROM_aki = support/glyph.js support/stroke.js support/spiroexpand.js support/spirokit.js parameters.js
+SUPPORT_FILES = $(SUPPORT_FILES_FROM_aki) support/point.js extract.js generate.js emptyfont.toml parameters.toml
+GLYPH_SEGMENTS = glyphs/common-shapes.aki glyphs/overmarks.aki glyphs/letters-unified-basic.aki glyphs/letters-unified-extended.aki  glyphs/numbers.aki glyphs/symbol-punctuation.aki glyphs/symbol-math.aki glyphs/symbol-geometric.aki glyphs/symbol-other.aki glyphs/symbol-letter.aki glyphs/autobuilds.aki
 SCRIPTS = $(SUPPORT_FILES) buildglyphs.js
-SCRIPTS_FROM_PATEL = $(SUPPORT_FILES_FROM_PATEL) buildglyphs.js
+SCRIPTS_FROM_aki = $(SUPPORT_FILES_FROM_aki) buildglyphs.js
 
-buildglyphs.js : buildglyphs.patel $(GLYPH_SEGMENTS)
-	$(PATEL) --strict $< -o $@
+buildglyphs.js : buildglyphs.aki $(GLYPH_SEGMENTS)
+	$(PATELC) --strict $< -o $@
 
-$(SUPPORT_FILES_FROM_PATEL) :
-	$(PATEL) --optimize --strict $< -o $@
-support/glyph.js : support/glyph.patel
-support/stroke.js : support/stroke.patel
-support/spirokit.js : support/spirokit.patel
-support/spiroexpand.js : support/spiroexpand.patel
-parameters.js : parameters.patel
+$(SUPPORT_FILES_FROM_aki) :
+	$(PATELC) --optimize --strict $< -o $@
+support/glyph.js : support/glyph.aki
+support/stroke.js : support/stroke.aki
+support/spirokit.js : support/spirokit.aki
+support/spiroexpand.js : support/spiroexpand.aki
+parameters.js : parameters.aki
 
 cleanscripts : 
-	-@rm $(SCRIPTS_FROM_PATEL)
+	-@rm $(SCRIPTS_FROM_aki)
 scripts : $(SCRIPTS)
