0ee8522820177f0eb0d7921215da406a584024bf
be5invis <belleve@typeof.net>
2015-12-17 18:00:16 +0800

    Final extension decision: .ptl.

--- makesupport.mk
+++ makesupport.mk
@@ -1,20 +1,20 @@
 PATELC = node ./node_modules/patel/bin/patel-c
 SUPPORT_FILES_FROM_aki = support/glyph.js support/stroke.js support/spiroexpand.js support/spirokit.js parameters.js
 SUPPORT_FILES = $(SUPPORT_FILES_FROM_aki) support/point.js extract.js generate.js emptyfont.toml parameters.toml
-GLYPH_SEGMENTS = glyphs/common-shapes.aki glyphs/overmarks.aki glyphs/letters-unified-basic.aki glyphs/letters-unified-extended.aki  glyphs/numbers.aki glyphs/symbol-punctuation.aki glyphs/symbol-math.aki glyphs/symbol-geometric.aki glyphs/symbol-other.aki glyphs/symbol-letter.aki glyphs/autobuilds.aki
+GLYPH_SEGMENTS = glyphs/common-shapes.ptl glyphs/overmarks.ptl glyphs/letters-unified-basic.ptl glyphs/letters-unified-extended.ptl  glyphs/numbers.ptl glyphs/symbol-punctuation.ptl glyphs/symbol-math.ptl glyphs/symbol-geometric.ptl glyphs/symbol-other.ptl glyphs/symbol-letter.ptl glyphs/autobuilds.ptl
 SCRIPTS = $(SUPPORT_FILES) buildglyphs.js
 SCRIPTS_FROM_aki = $(SUPPORT_FILES_FROM_aki) buildglyphs.js
 
-buildglyphs.js : buildglyphs.aki $(GLYPH_SEGMENTS)
+buildglyphs.js : buildglyphs.ptl $(GLYPH_SEGMENTS)
 	$(PATELC) --strict $< -o $@
 
 $(SUPPORT_FILES_FROM_aki) :
 	$(PATELC) --optimize --strict $< -o $@
-support/glyph.js : support/glyph.aki
-support/stroke.js : support/stroke.aki
-support/spirokit.js : support/spirokit.aki
-support/spiroexpand.js : support/spiroexpand.aki
-parameters.js : parameters.aki
+support/glyph.js : support/glyph.ptl
+support/stroke.js : support/stroke.ptl
+support/spirokit.js : support/spirokit.ptl
+support/spiroexpand.js : support/spiroexpand.ptl
+parameters.js : parameters.ptl
 
 cleanscripts : 
 	-@rm $(SCRIPTS_FROM_aki)
