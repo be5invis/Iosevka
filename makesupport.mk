519b7d830a516a6b63cf15804d13a8fe482c1e27
be5invis <belleve@typeof.net>
2015-12-21 06:06:41 +0800

    Remove simple-expand.

--- makesupport.mk
+++ makesupport.mk
@@ -1,5 +1,5 @@
 PATELC = node ./node_modules/patel/bin/patel-c
-SUPPORT_FILES_FROM_aki = support/glyph.js support/simple-expand.js support/spiroexpand.js support/spirokit.js parameters.js
+SUPPORT_FILES_FROM_aki = support/glyph.js support/spiroexpand.js support/spirokit.js parameters.js
 SUPPORT_FILES = $(SUPPORT_FILES_FROM_aki) support/point.js extract.js generate.js emptyfont.toml parameters.toml support/fairify.js
 GLYPH_SEGMENTS = glyphs/common-shapes.ptl glyphs/overmarks.ptl glyphs/letters-unified-basic.ptl glyphs/letters-unified-extended.ptl  glyphs/numbers.ptl glyphs/symbol-punctuation.ptl glyphs/symbol-math.ptl glyphs/symbol-geometric.ptl glyphs/symbol-other.ptl glyphs/symbol-letter.ptl glyphs/autobuilds.ptl
 SCRIPTS = $(SUPPORT_FILES) buildglyphs.js
@@ -11,7 +11,6 @@ buildglyphs.js : buildglyphs.ptl $(GLYPH_SEGMENTS)
 $(SUPPORT_FILES_FROM_aki) :
 	$(PATELC) --optimize --strict $< -o $@
 support/glyph.js : support/glyph.ptl
-support/simple-expand.js : support/simple-expand.ptl
 support/spirokit.js : support/spirokit.ptl
 support/spiroexpand.js : support/spiroexpand.ptl
 parameters.js : parameters.ptl
