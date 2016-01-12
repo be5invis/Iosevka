74cb767002e86cb65ad71465db07826ecbbbe47f
be5invis <belleve@typeof.net>
2016-01-13 05:26:50 +0800

    thrn on optimize again for buildglyphs.js

--- makesupport.mk
+++ makesupport.mk
@@ -13,7 +13,7 @@ SCRIPTS = $(SUPPORT_FILES) buildglyphs.js
 SCRIPTS_FROM_aki = $(SUPPORT_FILES_FROM_aki) buildglyphs.js
 
 buildglyphs.js : buildglyphs.ptl $(GLYPH_SEGMENTS)
-	$(PATELC) --strict $< -o $@
+	$(PATELC) --optimize --strict $< -o $@
 
 $(SUPPORT_FILES_FROM_aki) :
 	$(PATELC) --optimize --strict $< -o $@
