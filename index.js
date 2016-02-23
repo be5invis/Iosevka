092052098006113050868fca9e68dff5de82748a
be5invis <belleve@typeof.net>
2016-02-24 03:31:51 +0800

    r1.8.0

--- index.js
+++ index.js
@@ -42,8 +42,8 @@ var CodeSnippet = Vue.component('snippet', {
 var indexPage = new Vue({
 	el: 'body',
 	data: {
-		version: "1.7.5",
-		codename: "Galanodel-4",
+		version: "1.8.0",
+		codename: "Hermandine",
 		themes: ['color-light', 'color-dark'],
 		snippets: [],
 		snippet: 'javascript',
@@ -64,7 +64,8 @@ var indexPage = new Vue({
 			['0x1337F001',['cv13','cv14','cv15'], 'sub', '0'],
 			['~bitsRead',['cv16','cv17'], 'sub', '~'],
 			['*handler',['cv18','cv19'], 'sub', '*'],
-			['shared_ptr',['cv20','cv21'], 'sub', '_']
+			['shared_ptr',['cv20','cv21'], 'sub', '_'],
+			['ref:¶1.1',['cv22','cv23'], 'sub', '¶']
 		].map(function(item){
 			if(item instanceof Array && item[3]){
 				return [item[0].replace(new RegExp('[' + item[3] + ']', 'g'), '<b>$&</b>'), item[1], item[2]]
