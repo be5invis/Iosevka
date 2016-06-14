95b0ceee171da92717fbecba71ff8c0fd3fb392a
be5invis <belleve@typeof.net>
2016-06-14 10:06:57 +0800

    A massive refactor

--- makesupport.mk
+++ makesupport.mk
@@ -6,30 +6,22 @@ dist :
 	@- mkdir $@
 snapshot/assets :
 	@- mkdir $@
-	
+
 PATELC = node ./node_modules/patel/bin/patel-c
-SUPPORT_FILES_FROM_PTL = support/glyph.js support/spiroexpand.js support/spirokit.js parameters.js support/anchor.js support/point.js support/transform.js support/utils.js meta/aesthetics.js meta/naming.js meta/features.js
+
+GLYPH_SEGMENTS = glyphs/common-shapes.js glyphs/overmarks.js glyphs/letters-unified-basic.js glyphs/letters-unified-extended.js glyphs/numbers.js glyphs/symbol-punctuation.js glyphs/symbol-math.js glyphs/symbol-letter.js glyphs/symbol-geometric.js glyphs/symbol-other.js glyphs/symbol-braille.js glyphs/autobuilds.js
+
+SUPPORT_FILES_FROM_PTL = support/glyph.js support/spiroexpand.js support/spirokit.js parameters.js support/anchor.js support/point.js support/transform.js support/utils.js meta/aesthetics.js meta/naming.js meta/features.js $(GLYPH_SEGMENTS)
+
 SUPPORT_FILES = $(SUPPORT_FILES_FROM_PTL) generator.js emptyfont.toml parameters.toml support/fairify.js
-GLYPH_SEGMENTS = glyphs/common-shapes.ptl glyphs/overmarks.ptl glyphs/letters-unified-basic.ptl glyphs/letters-unified-extended.ptl  glyphs/numbers.ptl glyphs/symbol-punctuation.ptl glyphs/symbol-math.ptl glyphs/symbol-geometric.ptl glyphs/symbol-other.ptl glyphs/symbol-braille.ptl glyphs/symbol-letter.ptl glyphs/autobuilds.ptl
 SCRIPTS = $(SUPPORT_FILES) buildglyphs.js
 SCRIPTS_FROM_aki = $(SUPPORT_FILES_FROM_PTL) buildglyphs.js
 
 buildglyphs.js : buildglyphs.ptl $(GLYPH_SEGMENTS)
 	$(PATELC) --strict $< -o $@
 
-$(SUPPORT_FILES_FROM_PTL) :
+$(SUPPORT_FILES_FROM_PTL) : %.js : %.ptl meta/macros.ptl
 	$(PATELC) --optimize --strict $< -o $@
-support/anchor.js : support/anchor.ptl
-support/point.js : support/point.ptl
-support/transform.js : support/transform.ptl
-support/glyph.js : support/glyph.ptl
-support/spirokit.js : support/spirokit.ptl
-support/spiroexpand.js : support/spiroexpand.ptl
-support/utils.js : support/utils.ptl
-meta/aesthetics.js : meta/aesthetics.ptl
-meta/naming.js : meta/naming.ptl
-meta/features.js : meta/features.ptl
-parameters.js : parameters.ptl
 
 cleanscripts : 
 	-@rm $(SCRIPTS_FROM_aki)
