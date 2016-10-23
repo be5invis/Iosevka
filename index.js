06e9111ef37bdf440e999c4109d5bff70ecf81ea
be5invis <belleve@typeof.net>
2016-10-23 22:14:04 +0800

    r1.9.5

--- index.js
+++ index.js
@@ -42,8 +42,8 @@ var CodeSnippet = Vue.component('snippet', {
 var indexPage = new Vue({
 	el: 'body',
 	data: {
-		version: "1.9.4",
-		codename: "Ingrid-4",
+		version: "1.9.5",
+		codename: "Ingrid-5",
 		themes: ['color-light', 'color-dark'],
 		snippets: [],
 		snippet: 'javascript',
@@ -55,16 +55,16 @@ var indexPage = new Vue({
 				stress: 'agil*0',
 				type: 'wide',
 				terms: [
-					{tag:'off', desc:'Default'},
-					{tag:'ss01', desc:'Andale Mono Style'},
-					{tag:'ss02', desc:'Anonymous Pro Style'},
-					{tag:'ss03', desc:'Consolas Style'},
-					{tag:'ss04', desc:'Menlo Style'},
-					{tag:'ss05', desc:'Fira Mono Style'},
-					{tag:'ss06', desc:'Liberation Mono Style'},
-					{tag:'ss07', desc:'Monaco Style'},
-					{tag:'ss08', desc:'Pragmata Pro Style'},
-					{tag:'ss09', desc:'Source Code Pro Style'}
+					{ tag: 'off', desc: 'Default' },
+					{ tag: 'ss01', desc: 'Andale Mono Style' },
+					{ tag: 'ss02', desc: 'Anonymous Pro Style' },
+					{ tag: 'ss03', desc: 'Consolas Style' },
+					{ tag: 'ss04', desc: 'Menlo Style' },
+					{ tag: 'ss05', desc: 'Fira Mono Style' },
+					{ tag: 'ss06', desc: 'Liberation Mono Style' },
+					{ tag: 'ss07', desc: 'Monaco Style' },
+					{ tag: 'ss08', desc: 'Pragmata Pro Style' },
+					{ tag: 'ss09', desc: 'Source Code Pro Style' }
 				]
 			},
 			{
@@ -73,34 +73,34 @@ var indexPage = new Vue({
 				stress: '?',
 				type: 'narrow',
 				terms: [
-					{tag:'cv01', sample:'a'},
-					{tag:'cv02', sample:'a'},
-					{tag:'cv03', sample:'i'},
-					{tag:'cv04', sample:'i'},
-					{tag:'cv05', sample:'i'},
-					{tag:'cv06', sample:'i'},
-					{tag:'cv07', sample:'l'},
-					{tag:'cv08', sample:'l'},
-					{tag:'cv09', sample:'l'},
-					{tag:'cv10', sample:'l'},
-					{tag:'cv11', sample:'g'},
-					{tag:'cv12', sample:'g'},
-					{tag:'cv13', sample:'0'},
-					{tag:'cv14', sample:'0'},
-					{tag:'cv15', sample:'0'},
-					{tag:'cv16', sample:'~'},
-					{tag:'cv17', sample:'~'},
-					{tag:'cv18', sample:'*'},
-					{tag:'cv19', sample:'*'},
-					{tag:'cv20', sample:'_'},
-					{tag:'cv21', sample:'_'},
-					{tag:'cv22', sample:'¶'},
-					{tag:'cv23', sample:'¶'},
-					{tag:'cv24', sample:'g'},
-					{tag:'cv25', sample:'m'},
-					{tag:'cv26', sample:'m'},
-					{tag:'cv27', sample:'l'},
-					{tag:'cv28', sample:'l'}
+					{ tag: 'cv01', sample: 'a' },
+					{ tag: 'cv02', sample: 'a' },
+					{ tag: 'cv03', sample: 'i' },
+					{ tag: 'cv04', sample: 'i' },
+					{ tag: 'cv05', sample: 'i' },
+					{ tag: 'cv06', sample: 'i' },
+					{ tag: 'cv07', sample: 'l' },
+					{ tag: 'cv08', sample: 'l' },
+					{ tag: 'cv09', sample: 'l' },
+					{ tag: 'cv10', sample: 'l' },
+					{ tag: 'cv11', sample: 'g' },
+					{ tag: 'cv12', sample: 'g' },
+					{ tag: 'cv13', sample: '0' },
+					{ tag: 'cv14', sample: '0' },
+					{ tag: 'cv15', sample: '0' },
+					{ tag: 'cv16', sample: '~' },
+					{ tag: 'cv17', sample: '~' },
+					{ tag: 'cv18', sample: '*' },
+					{ tag: 'cv19', sample: '*' },
+					{ tag: 'cv20', sample: '_' },
+					{ tag: 'cv21', sample: '_' },
+					{ tag: 'cv22', sample: '¶' },
+					{ tag: 'cv23', sample: '¶' },
+					{ tag: 'cv24', sample: 'g' },
+					{ tag: 'cv25', sample: 'm' },
+					{ tag: 'cv26', sample: 'm' },
+					{ tag: 'cv27', sample: 'l' },
+					{ tag: 'cv28', sample: 'l' }
 				]
 			}
 		],
@@ -108,8 +108,8 @@ var indexPage = new Vue({
 		isBold: false,
 		isItalic: false
 	},
-	methods:{
-		stressSample: function(st, stress){
+	methods: {
+		stressSample: function (st, stress) {
 			return st.replace(new RegExp('[' + stress + ']', 'g'), '<b>$&</b>')
 		}
 	}
