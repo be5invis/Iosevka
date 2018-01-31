3e80c0008f05f9a543500991edeacbf70b097d9c
Renzhi Li <reli@microsoft.com>
2018-01-31 13:42:15 +0800

    v1.14.0

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var CompactPicker = Vue.component('compact-picker', {
-	template: '#compact-picker-template',
+var CompactPicker = Vue.component("compact-picker", {
+	template: "#compact-picker-template",
 	props: {
 		items: Array,
 		current: String,
@@ -13,8 +13,8 @@ var CompactPicker = Vue.component('compact-picker', {
 		}
 	}
 });
-var CompactToggle = Vue.component('compact-toggle', {
-	template: '#compact-toggle-template',
+var CompactToggle = Vue.component("compact-toggle", {
+	template: "#compact-toggle-template",
 	props: {
 		label: String,
 		on: Boolean,
@@ -29,8 +29,8 @@ var CompactToggle = Vue.component('compact-toggle', {
 		}
 	}
 });
-var CodeSnippet = Vue.component('snippet', {
-	template: '#code-snippet-template',
+var CodeSnippet = Vue.component("snippet", {
+	template: "#code-snippet-template",
 	props: {
 		name: String,
 		current: String
@@ -40,10 +40,10 @@ var CodeSnippet = Vue.component('snippet', {
 });
 
 var escapeHTML = (function() {
-	'use strict';
+	"use strict";
 	var matchHtmlRegExp = /["'&<>]/;
 	function escapeHTML(string) {
-		var str = '' + string;
+		var str = "" + string;
 		var match = matchHtmlRegExp.exec(str);
 
 		if (!match) {
@@ -51,30 +51,30 @@ var escapeHTML = (function() {
 		}
 
 		var escape;
-		var html = '';
+		var html = "";
 		var index = 0;
 		var lastIndex = 0;
 
 		for (index = match.index; index < str.length; index++) {
 			switch (str.charCodeAt(index)) {
 				case 34: // "
-					escape = '&quot;';
+					escape = "&quot;";
 					break;
 
 				case 38: // &
-					escape = '&amp;';
+					escape = "&amp;";
 					break;
 
 				case 39: // '
-					escape = '&#39;';
+					escape = "&#39;";
 					break;
 
 				case 60: // <
-					escape = '&lt;';
+					escape = "&lt;";
 					break;
 
 				case 62: // >
-					escape = '&gt;';
+					escape = "&gt;";
 					break;
 
 				default:
@@ -99,185 +99,195 @@ function TAG(ltag) {
 		return { ltag: ltag, s: s };
 	};
 }
-var arw2 = TAG('arrow2');
-var logc = TAG('logic');
-var cmpr = TAG('cmpr');
-var brst = TAG('brst');
-var dopr = TAG('dotoper');
+var arw2 = TAG("arrow2");
+var logc = TAG("logic");
+var cmpr = TAG("cmpr");
+var brst = TAG("brst");
+var dopr = TAG("dotoper");
 
 var indexPage = new Vue({
-	el: 'body',
+	el: "body",
 	data: {
-		version: '1.13.4',
-		codename: 'Marisa-4',
-		themes: ['color-light', 'color-dark'],
+		version: "1.14.0",
+		codename: "Nagisa",
+		themes: ["color-light", "color-dark"],
 		snippets: [],
-		snippet: 'javascript',
-		theme: 'color-dark',
+		snippet: "javascript",
+		theme: "color-dark",
 		ligationSamples: [
 			[
-				arw2('-<<'),
-				arw2('-<'),
-				arw2('-<-'),
-				'<--',
-				'<---',
-				arw2('<<-'),
-				'<-',
-				'->',
-				arw2('->>'),
-				'-->',
-				'--->',
-				arw2('->-'),
-				arw2('>-'),
-				arw2('>>-'),
-				'<->',
-				'<-->',
-				'<--->',
-				'<---->',
-				'<!--'
+				arw2("-<<"),
+				arw2("-<"),
+				arw2("-<-"),
+				"<--",
+				"<---",
+				arw2("<<-"),
+				"<-",
+				"->",
+				arw2("->>"),
+				"-->",
+				"--->",
+				arw2("->-"),
+				arw2(">-"),
+				arw2(">>-"),
+				"<->",
+				"<-->",
+				"<--->",
+				"<---->",
+				"<!--"
 			],
 			[
-				arw2('=<<'),
-				arw2('=<'),
-				arw2('=<='),
-				'<==',
-				'<===',
-				arw2('<<='),
-				cmpr('<='),
-				'=>',
-				arw2('=>>'),
-				'==>',
-				'===>',
-				arw2('=>='),
-				cmpr('>='),
-				arw2('>>='),
-				'<=>',
-				'<==>',
-				'<===>',
-				'<====>',
-				'<!---'
+				arw2("=<<"),
+				arw2("=<"),
+				arw2("=<="),
+				"<==",
+				"<===",
+				arw2("<<="),
+				"<=",
+				"=>",
+				arw2("=>>"),
+				"==>",
+				"===>",
+				arw2("=>="),
+				">=",
+				arw2(">>="),
+				"<=>",
+				"<==>",
+				"<===>",
+				"<====>",
+				"<!---"
 			],
 			[
-				'<----------------',
-				'---------------->',
-				'<===============>',
-				'a:b',
-				'a::b',
-				'a:::b',
-				logc('a\\/b'),
-				logc('a/\\b')
+				"<----------------",
+				"---------------->",
+				"<===============>",
+				"a:b",
+				"a::b",
+				"a:::b",
+				logc("a\\/b"),
+				logc("a/\\b")
 			],
 			[
-				':=',
-				':-',
-				':+',
-				'<*',
-				'<*>',
-				'*>',
-				'<|',
-				'<|>',
-				'|>',
-				dopr('<.'),
-				dopr('<.>'),
-				dopr('.>'),
-				'+:',
-				'-:',
-				'=:',
-				'<******>',
-				brst('(* comm *)')
+				":=",
+				":-",
+				":+",
+				"<*",
+				"<*>",
+				"*>",
+				"<|",
+				"<|>",
+				"|>",
+				dopr("<."),
+				dopr("<.>"),
+				dopr(".>"),
+				"+:",
+				"-:",
+				"=:",
+				"<******>",
+				brst("(* comm *)"),
+				"==",
+				"!=",
+				"===",
+				"!=="
 			]
 		],
 		ligationSets: [
-			{ tag: 'calt', desc: 'Default setting in text editors', group: [] },
-			{ tag: 'XFST', tagName: 'XML0, XFS0, XFST', desc: 'ML, OCaml, F#, F*', group: ['brst', 'logic', 'ml'] },
-			{ tag: 'SWFT', tagName: 'SWFT, XPTL', desc: 'Swift, PatEL', group: ['arrow2'] },
+			{ tag: "calt", desc: "Default setting in text editors", group: [] },
 			{
-				tag: 'XHS0',
-				tagName: 'XHS0, XIDR, XELM, PURS',
-				desc: 'Haskell, Idris, Elm, PureScript',
-				group: ['arrow2', 'dotoper', 'logic']
+				tag: "XFST",
+				tagName: "XML0, XFS0, XFST",
+				desc: "ML, OCaml, F#, F*",
+				group: ["brst", "logic", "ml"]
+			},
+			{ tag: "SWFT", tagName: "SWFT, XPTL", desc: "Swift, PatEL", group: ["arrow2"] },
+			{
+				tag: "XHS0",
+				tagName: "XHS0, XIDR, XELM, PURS",
+				desc: "Haskell, Idris, Elm, PureScript",
+				group: ["arrow2", "dotoper", "logic"]
 			},
 			{
-				tag: 'XV00',
-				tagName: 'XV00',
-				desc: 'Coq',
-				group: ['arrow2', 'dotoper', 'logic', 'brst']
+				tag: "XV00",
+				tagName: "XV00",
+				desc: "Coq",
+				group: ["arrow2", "dotoper", "logic", "brst"]
 			}
 		],
 		opentypeSamples: [
 			{
-				title: 'Stylistic Sets',
-				sampleText: '@real fox.quick(h){ *is_brown && it_jumps_over(doges.lazy) } 0123456789',
-				stress: 'agil*0_{}@3',
-				type: 'wide',
+				title: "Stylistic Sets",
+				sampleText:
+					"@real fox.quick(h){ *is_brown && it_jumps_over(doges.lazy) } 0123456789",
+				stress: "agil*0_{}@3",
+				type: "wide",
 				terms: [
-					{ tag: 'off', desc: 'Default' },
-					{ tag: 'ss01', desc: 'Andale Mono Style' },
-					{ tag: 'ss02', desc: 'Anonymous Pro Style' },
-					{ tag: 'ss03', desc: 'Consolas Style' },
-					{ tag: 'ss04', desc: 'Menlo Style' },
-					{ tag: 'ss05', desc: 'Fira Mono Style' },
-					{ tag: 'ss06', desc: 'Liberation Mono Style' },
-					{ tag: 'ss07', desc: 'Monaco Style' },
-					{ tag: 'ss08', desc: 'Pragmata Pro Style' },
-					{ tag: 'ss09', desc: 'Source Code Pro Style' },
-					{ tag: 'ss10', desc: 'Envy Code R Style' },
-					{ tag: 'ss11', desc: 'X Windows Fixed Style' }
+					{ tag: "off", desc: "Default" },
+					{ tag: "ss01", desc: "Andale Mono Style" },
+					{ tag: "ss02", desc: "Anonymous Pro Style" },
+					{ tag: "ss03", desc: "Consolas Style" },
+					{ tag: "ss04", desc: "Menlo Style" },
+					{ tag: "ss05", desc: "Fira Mono Style" },
+					{ tag: "ss06", desc: "Liberation Mono Style" },
+					{ tag: "ss07", desc: "Monaco Style" },
+					{ tag: "ss08", desc: "Pragmata Pro Style" },
+					{ tag: "ss09", desc: "Source Code Pro Style" },
+					{ tag: "ss10", desc: "Envy Code R Style" },
+					{ tag: "ss11", desc: "X Windows Fixed Style" }
 				]
 			},
 			{
-				title: 'Character Variants',
-				sampleText: '',
-				stress: '?',
-				type: 'narrow',
+				title: "Character Variants",
+				sampleText: "",
+				stress: "?",
+				type: "narrow",
 				terms: [
-					{ tag: 'cv01', sample: 'a' },
-					{ tag: 'cv02', sample: 'a' },
-					{ tag: 'cv03', sample: 'i' },
-					{ tag: 'cv04', sample: 'i' },
-					{ tag: 'cv05', sample: 'i' },
-					{ tag: 'cv06', sample: 'i' },
-					{ tag: 'cv07', sample: 'l' },
-					{ tag: 'cv08', sample: 'l' },
-					{ tag: 'cv09', sample: 'l' },
-					{ tag: 'cv10', sample: 'l' },
-					{ tag: 'cv11', sample: 'g' },
-					{ tag: 'cv12', sample: 'g' },
-					{ tag: 'cv13', sample: '0' },
-					{ tag: 'cv14', sample: '0' },
-					{ tag: 'cv15', sample: '0' },
-					{ tag: 'cv16', sample: '~' },
-					{ tag: 'cv17', sample: '~' },
-					{ tag: 'cv18', sample: '*' },
-					{ tag: 'cv19', sample: '*' },
-					{ tag: 'cv20', sample: '_' },
-					{ tag: 'cv21', sample: '_' },
-					{ tag: 'cv22', sample: '¶' },
-					{ tag: 'cv23', sample: '¶' },
-					{ tag: 'cv24', sample: 'g' },
-					{ tag: 'cv25', sample: 'm' },
-					{ tag: 'cv26', sample: 'm' },
-					{ tag: 'cv27', sample: 'l' },
-					{ tag: 'cv28', sample: 'l' },
-					{ tag: 'cv29', sample: '^' },
-					{ tag: 'cv30', sample: '^' },
-					{ tag: 'cv31', sample: '@' },
-					{ tag: 'cv32', sample: '@' },
-					{ tag: 'cv33', sample: '@' },
-					{ tag: 'cv34', sample: 'ß' },
-					{ tag: 'cv35', sample: 'ß' },
-					{ tag: 'cv36', sample: '{' },
-					{ tag: 'cv37', sample: '{' },
-					{ tag: 'cv38', sample: '$' },
-					{ tag: 'cv39', sample: '$' },
-					{ tag: 'cv40', sample: 't' },
-					{ tag: 'cv41', sample: 't' },
-					{ tag: 'cv42', sample: 'Q' },
-					{ tag: 'cv43', sample: 'Q' },
-					{ tag: 'cv44', sample: '#' },
-					{ tag: 'cv45', sample: '#' },
-					{ tag: 'cv46', sample: '3' },
-					{ tag: 'cv47', sample: '3' }
+					{ tag: "cv01", sample: "a" },
+					{ tag: "cv02", sample: "a" },
+					{ tag: "cv03", sample: "i" },
+					{ tag: "cv04", sample: "i" },
+					{ tag: "cv05", sample: "i" },
+					{ tag: "cv06", sample: "i" },
+					{ tag: "cv07", sample: "l" },
+					{ tag: "cv08", sample: "l" },
+					{ tag: "cv09", sample: "l" },
+					{ tag: "cv10", sample: "l" },
+					{ tag: "cv11", sample: "g" },
+					{ tag: "cv12", sample: "g" },
+					{ tag: "cv13", sample: "0" },
+					{ tag: "cv14", sample: "0" },
+					{ tag: "cv15", sample: "0" },
+					{ tag: "cv16", sample: "~" },
+					{ tag: "cv17", sample: "~" },
+					{ tag: "cv18", sample: "*" },
+					{ tag: "cv19", sample: "*" },
+					{ tag: "cv20", sample: "_" },
+					{ tag: "cv21", sample: "_" },
+					{ tag: "cv22", sample: "¶" },
+					{ tag: "cv23", sample: "¶" },
+					{ tag: "cv24", sample: "g" },
+					{ tag: "cv25", sample: "m" },
+					{ tag: "cv26", sample: "m" },
+					{ tag: "cv27", sample: "l" },
+					{ tag: "cv28", sample: "l" },
+					{ tag: "cv29", sample: "^" },
+					{ tag: "cv30", sample: "^" },
+					{ tag: "cv31", sample: "@" },
+					{ tag: "cv32", sample: "@" },
+					{ tag: "cv33", sample: "@" },
+					{ tag: "cv34", sample: "ß" },
+					{ tag: "cv35", sample: "ß" },
+					{ tag: "cv36", sample: "{" },
+					{ tag: "cv37", sample: "{" },
+					{ tag: "cv38", sample: "$" },
+					{ tag: "cv39", sample: "$" },
+					{ tag: "cv40", sample: "t" },
+					{ tag: "cv41", sample: "t" },
+					{ tag: "cv42", sample: "Q" },
+					{ tag: "cv43", sample: "Q" },
+					{ tag: "cv44", sample: "#" },
+					{ tag: "cv45", sample: "#" },
+					{ tag: "cv46", sample: "3" },
+					{ tag: "cv47", sample: "3" }
 				]
 			}
 		],
@@ -287,15 +297,15 @@ var indexPage = new Vue({
 	},
 	methods: {
 		stressSample: function(st, stress) {
-			return st.replace(new RegExp('[' + stress + ']', 'g'), '<b>$&</b>');
+			return st.replace(new RegExp("[" + stress + "]", "g"), "<b>$&</b>");
 		},
 		elabSample: function(g, s) {
-			if (typeof s === 'string') {
-				return '<em>' + escapeHTML(s) + '</em>';
+			if (typeof s === "string") {
+				return "<em>" + escapeHTML(s) + "</em>";
 			} else if (s.ltag && g.indexOf(s.ltag) >= 0) {
-				return '<em>' + escapeHTML(s.s) + '</em>';
+				return "<em>" + escapeHTML(s.s) + "</em>";
 			} else {
-				return '<s>' + escapeHTML(s.s) + '</s>';
+				return "<s>" + escapeHTML(s.s) + "</s>";
 			}
 		}
 	}
