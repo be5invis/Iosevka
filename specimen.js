561b84db452a5992a73249c8f07a49398059d41e
Carlos Rodriguez Guette <carlosrogue@gmail.com>
2020-03-03 03:42:18 +0100

    Update spanish sample

--- specimen.js
+++ specimen.js
@@ -261,7 +261,7 @@ var sampleSentences = [
 		code: "es",
 		lang: "Spanish",
 		sample:
-			"Benjamín pidió una bebida de kiwi y fresa; Noé, sinvergüenza, la más exquisita champaña del menú."
+			"Benjamín pidió una bebida de kiwi y fresa; Noé, sin vergüenza, la más exquisita champaña del menú."
 	},
 	{ code: "tr", lang: "Turkish", sample: "Pijamalı hasta yağız şoföre çabucak güvendi." },
 	{
