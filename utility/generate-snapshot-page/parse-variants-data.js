dd49b745329545013768c3551eb25beec24e86f6
Belleve Invis <belleve@typeof.net>
2020-03-06 17:30:21 -0800

    Add SS14, CV89 and CV90. Fixes #434.

--- utility/generate-snapshot-page/parse-variants-data.js
+++ utility/generate-snapshot-page/parse-variants-data.js
@@ -49,7 +49,7 @@ function getCvData(variants) {
 
 function rankOf(initialChar) {
 	if ("A" <= initialChar && initialChar <= "Z") return 3;
-	if (("a" <= initialChar && initialChar <= "z") || initialChar === "ß") return 2;
+	if ("a" <= initialChar && initialChar <= "z") return 2;
 	if ("0" <= initialChar && initialChar <= "9") return 1;
 	return 0;
 }
