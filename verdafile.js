ca995d73341401759463625fefb742c538463d94
Belleve Invis <belleve@typeof.net>
2020-03-28 10:54:12 -0700

    Make the release note file generate more efficiently

--- verdafile.js
+++ verdafile.js
@@ -641,11 +641,18 @@ const ChangeFileList = oracle.make(
 	() => `release:change-file-list`,
 	target => FileList({ under: "changes", pattern: "*.md" })(target)
 );
-const ReleaseNotes = task(`release:release-note`, async target => {
-	const [version] = await target.need(Version, UtilScripts, de(ARCHIVE_DIR));
-	const [changeFiles] = await target.need(ChangeFileList());
-	await target.need(changeFiles.map(fu));
-	await run("node", "utility/generate-release-note/index", version);
+const ReleaseNotesFile = file.make(
+	version => `${ARCHIVE_DIR}/release-notes-${version}.md`,
+	async (t, out, version) => {
+		await t.need(UtilScripts, de(ARCHIVE_DIR));
+		const [changeFiles] = await t.need(ChangeFileList());
+		await t.need(changeFiles.map(fu));
+		await run("node", "utility/generate-release-note/index", version, out.full);
+	}
+);
+const ReleaseNotes = task(`release:release-note`, async t => {
+	const [version] = await t.need(Version);
+	await t.need(ReleaseNotesFile(version));
 });
 
 phony(`clean`, async () => {
