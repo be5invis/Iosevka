040c1ff08c3f2e74f68c86ed7ed51bc8d0402bc7
be5invis <belleve@typeof.net>
2020-11-10 20:52:55 -0800

    Cleanup code for orthographic italics. Now variant selector could influence the upright and italic counterpart individually -- though currently they aren't.

--- font-src/support/variant-data.js
+++ font-src/support/variant-data.js
@@ -119,8 +119,6 @@ class PrimeVariant {
 		this.description = cfg.description;
 		this.rank = cfg.rank;
 		this.selector = cfg.selector;
-		this.selectorUpright = cfg.selectorUpright;
-		this.selectorItalic = cfg.selectorItalic;
 	}
 	resolveFor(para, gn) {
 		let vs = {};
@@ -129,11 +127,6 @@ class PrimeVariant {
 	}
 	resolve(para, vs) {
 		Object.assign(vs, this.selector);
-		if (para.isItalic) {
-			Object.assign(vs, this.selectorItalic);
-		} else {
-			Object.assign(vs, this.selectorUpright);
-		}
 	}
 }
 
