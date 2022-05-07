6b94447100cebac46545227cb21d0a64b06b300e
be5invis <belleve@typeof.net>
2022-05-07 13:04:34 -0700

    Perf optimization of caching

--- font-src/gen/caching/index.js
+++ font-src/gen/caching/index.js
@@ -4,7 +4,7 @@ const fs = require("fs-extra");
 const zlib = require("zlib");
 const { encode, decode } = require("@msgpack/msgpack");
 
-const Edition = 13;
+const Edition = 14;
 const MAX_AGE = 5;
 
 class GfEntry {
