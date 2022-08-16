851254ea6ad1f9bb9e203710f05adb4d4879bd5f
be5invis <belleve@typeof.net>
2022-08-16 02:31:49 -0700

    Update image sample

--- utility/generate-samples/templates/stylistic-set.mjs
+++ utility/generate-samples/templates/stylistic-set.mjs
@@ -1,11 +1,13 @@
 import * as themes from "../themes/index.mjs";
 
+// prettier-ignore
 const ssStrings = [
 	["ABC.DEF.GHI.JKL.MNO.PQRS.TUV.WXYZ", "abc.def.ghi.jkl.mno.pqrs.tuv.wxyz"],
 	["¢ ſß ΓΛΔ αδιλμξ КУЗЯЖэльфязычникж", "float il1[]={1-2/3.4,5+6=7/8%90};"],
 	["1234567890 ,._-+= >< ¯-¬_ >~–÷+×<", "{}[]()<>`+-=$*/#_%^@\\&|~?'\" !,.;:"],
-	["!iIlL17|¦ coO08BbDQ $5SZ2zsz 96G&", [..."dbqp E3 g9q CGQ vvw VVW /V ", "<=", " ", ">="]]
+	["!iIlL17|¦ coO08BbDQ $5SZ2zsz 96G&", [..."dbqp E3 g9q CGQ vvw VVW ", "<=", " ", "!=", " ", ">="]]
 ];
+
 function* makeSample(theme, lbm, features, hotChars) {
 	const hotCharSet = new Set(hotChars);
 	for (const row of ssStrings) {
