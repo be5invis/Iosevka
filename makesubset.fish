e9182065963f27871aa23f8fddf23f4c36819953
Daniel Flanagan <daniel@lyte.dev>
2022-11-22 04:36:44 -0600

    Add clarity to subset script and update web font

--- makesubset.fish
+++ makesubset.fish
@@ -2,18 +2,19 @@
 
 # requires python packages: fonttools brotli
 #
-# TODO: need to figure out how to preserve ligatures
+# TODO: need to figure out how to preserve ligatures?
+
 
 for f in ./dist/iosevkalyteweb/woff2/*.woff2
-	not string match "*.subset.woff2" $f >/dev/null && set -a _files $f
+	not string match "*.subset.woff2" $f >/dev/null && set -a files $f
 end
 
 if command -v parallel >/dev/null
 	echo "Starting GNU Parallel. It may take a bit for jobs to start finishing."
-	parallel --bar --progress --eta pyftsubset --name-IDs+=0,4,6 --text-file=./glyphs.txt --flavor=woff2 ::: $_files
+	parallel --bar --progress --eta pyftsubset --name-IDs+=0,4,6 --text-file=./subset-glyphs.txt --flavor=woff2 ::: $files
 else
 	set fish_trace on
 	for f in $_files
-		pyftsubset $f --name-IDs+=0,4,6 --text-file=./glyphs.txt --flavor=woff2
+		pyftsubset $f --name-IDs+=0,4,6 --text-file=./subset-glyphs.txt --flavor=woff2
 	end
 end
