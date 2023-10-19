abf171549576d646ba50f53a4765ddf46ae4820c
be5invis <belleve@typeof.net>
2023-10-18 17:00:49 -0700

    #2051 fix

--- utility/export-data/coverage-export/block-data.mjs
+++ utility/export-data/coverage-export/block-data.mjs
@@ -3,13 +3,7 @@ import UnicodeDataIndex from "@unicode/unicode-15.0.0";
 export async function collectBlockData() {
 	const BlockData = [
 		[[0xe0a0, 0xe0df], "Private Use Area — Powerline"],
-		[[0xee00, 0xee3f], "Private Use Area — Progress Bar"],
-		// Missing ranges in UnicodeDataIndex
-		[[0x10780, 0x107bf], "Latin Extended-F"],
-		[[0x1df00, 0x1dfff], "Latin Extended-G"],
-		[[0x1e030, 0x1e08f], "Cyrillic Extended-D"],
-		[[0x1fa70, 0x1faff], "Symbols and Pictographs Extended-A"],
-		[[0x1fb00, 0x1fbff], "Symbols for Legacy Computing"]
+		[[0xee00, 0xee3f], "Private Use Area — Progress Bar"]
 	];
 
 	for (const id of UnicodeDataIndex.Block) {
