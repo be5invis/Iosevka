23c3565b77c00c7d4a740ea554a9889d36dbf623
be5invis <belleve@typeof.net>
2024-09-11 18:46:55 -0700

    Update unicode dataset version to 16.0

--- tools/data-export/src/coverage-export/block-data.mjs
+++ tools/data-export/src/coverage-export/block-data.mjs
@@ -1,16 +1,15 @@
-import UnicodeDataIndex from "@unicode/unicode-15.1.0";
+import UnicodeDataIndex from "@unicode/unicode-16.0.0";
 
 export async function collectBlockData() {
 	const BlockData = [
 		[[0xe0a0, 0xe0df], "Private Use Area — Powerline"],
 		[[0xee00, 0xee0f], "Private Use Area — Progress Bar"],
 		[[0xef10, 0xef1f], "Private Use Area — Iosevka Private Dingbats"],
-		[[0x1cc00, 0x1ceaf], "Symbols for Legacy Computing Supplement (Proposed)"],
 	];
 
 	for (const id of UnicodeDataIndex.Block) {
 		if (!id || /Private_Use_Area/.test(id) || /undefined/.test(id)) continue;
-		const rangesModule = await import(`@unicode/unicode-15.1.0/Block/${id}/ranges.js`);
+		const rangesModule = await import(`@unicode/unicode-16.0.0/Block/${id}/ranges.js`);
 		const rg = rangesModule.default;
 		BlockData.push([[rg[0].begin, rg[0].end - 1], id.replace(/_/g, " ")]);
 	}
